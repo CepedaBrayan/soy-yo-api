f4168133d0719ba5090c500e79011113bd41d489
Brayan Rivera Cepeda <riveracepedabrayan@gmail.com>
2022-07-07 11:40:49 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('test-soyyo');
+  app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('Test SOYYO')
     .setVersion('1.0')
